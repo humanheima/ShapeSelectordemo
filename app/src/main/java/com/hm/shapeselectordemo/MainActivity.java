2cad14246b78f28561c796c9027690b611125e5e
p_dmweidu <dumingwei@yuewen.com>
2024-01-25 19:53:40 +0800

    使用AndroidX.

--- app/src/main/java/com/hm/shapeselectordemo/MainActivity.java
+++ app/src/main/java/com/hm/shapeselectordemo/MainActivity.java
@@ -1,31 +1,14 @@
 package com.hm.shapeselectordemo;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
-import android.widget.ScrollView;
-import android.widget.Toast;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.Button1)
-    Button Button1;
-    @BindView(R.id.activity_main)
-    ScrollView activityMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.Button1)
-    public void onClick() {
-        Toast.makeText(this, "button1 was clicked", Toast.LENGTH_SHORT).show();
-    }
 }
